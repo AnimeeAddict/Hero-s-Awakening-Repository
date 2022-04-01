5c3ca5e95fb4e445d8a3235e02c66a8006560630
BrandonXiong004 <94199623+BrandonXiong004@users.noreply.github.com>
2022-04-01 15:22:52 -0700

    commit 4/1/2022 3:22pm

---
+++ main.js
@@ -0,0 +1,21 @@
+window.onload = function(){//Directly checks to ensure JavaScript loads first before any code is processed. (This part of code is referenced by Choice of Adventure replit) (https://interactive-story-cartwebapp-8.cartwebapp.repl.co/)
+    const story = {//this is the story object
+
+}
+
+//Variables and function algorithm
+const decisions = ['prologue']; //this holds all variables of text and choices
+
+//UI variables
+var enterBtn = document.getElementById('enterButton');
+var startOverBtn = document.getElementById('startOverButton');
+var interactArea = document.getElementById('container');
+var uiButtons = document.getElementById('uiButtons');
+
+//username select
+let ;
+
+function 
+
+
+
